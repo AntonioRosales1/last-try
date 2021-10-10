4525006ad0cb393813f89789be195340cbb1e80c
elrlu <elrlu@LAPTOP-09KVR600>
2021-10-09 18:20:44 -0700

    js

--- app.js
+++ app.js
@@ -15,8 +15,22 @@ btn.addEventListener("click", secondFunction);
 
 
 var field = document.querySelector('textareas');
+var backUp = feild.getAttribute('placeholder');
+var btnOne = document.querySelector('.btnOne');
+var clear = document.getElementById('clear');
 
 field.onfocus = function(){
     this.setAttribute('placeholder', '');
     this.style.borderColor = '#333';
+    btn.style.display = 'block';
+}
+
+field.onblur = function(){
+    this.setAttribute('placeholder', backUp);
+    this.style.borderColor = '#aaa';
+}
+
+clear.onclick = function(){
+    btn.style.display = 'none';
+    field.value = '';
 }
